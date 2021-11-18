8e3655de7f403e93c5438decbcf465e553812fed
Shoider <82180734+Shoider@users.noreply.github.com>
2021-11-18 02:07:53 -0600

    Documentacion

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.mycompany.poop10","l":"Cuenta"},{"p":"com.mycompany.poop10","l":"DepositoMaximoExcedidoException"},{"p":"com.mycompany.poop10","l":"Main"},{"p":"com.mycompany.poop10","l":"MaximoDeRetirosException"},{"p":"com.mycompany.poop10","l":"SaldoInsuficienteException"}];updateSearchResults();
